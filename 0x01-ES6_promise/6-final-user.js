b0236d2f14e5fd48fc4ebc2c3b0c45a7db4227ae
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-07-25 01:34:04 +0300

    task-6.2

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function formatResult(result) {
-  return {
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  };
-}
-
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, photoPromise])
-    .then((results) => results.map(formatResult));
+  return Promise.allSettled([userPromise, photoPromise]).then((results) => {
+    const result = [];
+    results.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return result;
+  });
 }
