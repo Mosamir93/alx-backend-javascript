9ab68bd54a3e73d4b52bfd0a88022f9beea3f1f8
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-07-30 15:50:49 +0300

    task-4.1

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,8 @@
 export default function updateStudentGradeByCity(list, city, grades) {
-  return list
-    .filter((student) => student.location === city)
+  const cityStudents = list.filter((student) => student.location === city)
     .map((student) => {
-      const gradeRecord = grades.find(grade => grade.studentId === student.id);
-      return {
-        ...student,
-        grade: gradeRecord ? gradeRecord.grade : 'N/A',
-      };
+      const gradeRecord = grades.find((grade) => grade.studentId === student.id);
+      return { ...student, grade: gradeRecord ? gradeRecord.grade : 'N/A' };
     });
+  return cityStudents;
 }
