5b9c93c20a52384c67984c9bb73592b3d1a15859
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-07-30 16:52:25 +0300

    task-8.4

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0 || startString === undefined) {
+  if (startString.length === 0 || typeof startString !== 'string') {
     return '';
   }
   return [...set]
-    .filter((elem) => (elem !== undefined ? elem.startsWith(startString) : ''))
-    .map((elem) => (elem !== undefined ? elem.slice(startString.length) : ''))
+    .filter((elem) => elem !== undefined && elem.startsWith(startString))
+    .map((elem) => elem.slice(startString.length))
     .join('-');
 }
