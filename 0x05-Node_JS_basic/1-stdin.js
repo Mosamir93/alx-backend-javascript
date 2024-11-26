e175b24ee786d80cfc22aeb220eb68a26077dd7f
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-26 23:30:58 +0200

    task-1.2

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('data', () => {
-  const name = process.stdin.read();
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
   if (name) {
     process.stdout.write(`Your name is: ${name}\n`);
   }
