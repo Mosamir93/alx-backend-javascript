11917c19c2b10e10d5c4c1f43aaabf2dff60a4b5
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-27 01:30:31 +0200

    task-5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const fs = require('fs').promises;
+
+const hostname = '127.0.0.1';
+const port = '1245';
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const studentCount = `Number of students: ${lines.length - 1}`;
+    const fields = {};
+
+    lines.slice(1).forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstname);
+    });
+
+    const fieldDetails = Object.entries(fields)
+      .map(([field, names]) => `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`)
+      .join('\n');
+
+    return `${studentCount}\n${fieldDetails}`;
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    countStudents(process.argv[2])
+      .then((result) => {
+        res.write('This is the list of our students\n');
+        res.end(result);
+      })
+      .catch((err) => {
+        res.end(`${err.message}`);
+      });
+  }
+});
+
+app.listen(port, hostname);
+
+module.exports = app;
