2217845d9ed2d15d51abe432400c07a55c964904
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-11-28 22:42:45 +0200

    task-6.1

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,10 +2,10 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-    it('should return a resolved promise with correct data when success is true', () => {
+    it('should return a resolved promise with correct data when success is true', (done) => {
         getPaymentTokenFromAPI(true).then((response) => {
             expect(response).to.deep.equal({ data: 'Successful response from the API' });
             done();
-        }).catch((err) => done(err));
+        })
     })
 })
